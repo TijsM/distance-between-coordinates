4821a5606415d18b644460ad5da3bdf60dc4b2d6
Tijs Martens <tijs@bothrs.com>
2022-03-23 09:21:39 +0100

    feat: add unit

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 export type Coordinate = {
   lat: number;
   lon: number;
-  altitude?: number | undefined;
-  time?: Date;
 };
 
+type Unit = 'km' | 'mile';
+
 // source of algorithm: https://www.geodatasource.com/developers/javascript
-export function getDistanceBetweenTwoPoints(cord1: Coordinate, cord2: Coordinate) {
+export function getDistanceBetweenTwoPoints(
+  cord1: Coordinate,
+  cord2: Coordinate,
+  unit: Unit = 'km',
+) {
   if (cord1.lat === cord2.lat && cord1.lon === cord2.lon) {
     return 0;
   }
@@ -17,7 +21,9 @@ export function getDistanceBetweenTwoPoints(cord1: Coordinate, cord2: Coordinate
   const theta = cord1.lon - cord2.lon;
   const radtheta = (Math.PI * theta) / 180;
 
-  let dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+  let dist =
+    Math.sin(radlat1) * Math.sin(radlat2) +
+    Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
 
   if (dist > 1) {
     dist = 1;
@@ -26,12 +32,14 @@ export function getDistanceBetweenTwoPoints(cord1: Coordinate, cord2: Coordinate
   dist = Math.acos(dist);
   dist = (dist * 180) / Math.PI;
   dist = dist * 60 * 1.1515;
-  dist = dist * 1.609344; // convert miles to km
+  if (unit === 'km') {
+    dist = dist * 1.609344;
+  }
 
   return dist;
 }
 
-export function getTotalDistance(coordinates: Coordinate[]) {
+export function getTotalDistance(coordinates: Coordinate[], unit: Unit) {
   coordinates = coordinates.filter((cord) => {
     if (cord.lat && cord.lon) {
       return true;
@@ -49,10 +57,17 @@ export function getTotalDistance(coordinates: Coordinate[]) {
   }
 
   for (let i = 0; i < coordinates.length - 2; i++) {
-    if (!coordinates[i].lon || !coordinates[i].lat || !coordinates[i + 1].lon || !coordinates[i + 1].lat) {
+    if (
+      !coordinates[i].lon ||
+      !coordinates[i].lat ||
+      !coordinates[i + 1].lon ||
+      !coordinates[i + 1].lat
+    ) {
       totalDistance = totalDistance;
     }
-    totalDistance = totalDistance + getDistanceBetweenTwoPoints(coordinates[i], coordinates[i + 1]);
+    totalDistance =
+      totalDistance +
+      getDistanceBetweenTwoPoints(coordinates[i], coordinates[i + 1], unit);
   }
 
   return totalDistance.toFixed(2);
